4498d06acc1751ee128a21bcb4ef322bd5e50a74
iluzych <ieva.daugvilaite@gmail.com>
2021-05-06 17:17:28 +0300

    string lenght

--- js/03-strings.js
+++ js/03-strings.js
@@ -67,3 +67,10 @@ inicijavimo budai:
                 </nav>
             </header>`;
  console.log(H2);
+
+ const demo = 'asd ${imgSrc} asd';
+ console.log(demo);
+
+ const hi = 'Good morning!';
+ const hiSize = hi.length;
+ console.log(hi, hiSize);
