2bd90773cda480e588c2ccbe05f26f8e28870227
iluzych <ieva.daugvilaite@gmail.com>
2021-05-06 18:59:45 +0300

    Ternary

---
+++ js/09-ternary.js
@@ -0,0 +1,35 @@
+/*
+TERNARY OPERATOR
+
+klausimas ? teigiama : neigiama
+
+*/
+
+const arLyja = true;
+
+const sketis = arLyja ? 'pasiimk' : 'nereikia';   // pasiimk /nereikia
+console.log(sketis);
+
+const s1 = 4 < 2 
+                ? 'daugiau' 
+                : 'ne daugiau';
+console.log(s1);
+
+
+let s2 = '';
+if (4 < 2) {
+    s2 = 'daugiau';
+} else {
+    s2 = 'ne daugiau';
+}
+console.log(s2);
+
+/*
+Nestintas ternary
+*/
+
+const color = 'red';
+const light = true;
+
+const x1 = color === 'blue' ? light ? 'Sviesiai melyna' : 'Tamsiai melyna' : color === 'red' ? 'Raudona' : 'Nezinoma spalva';
+console.log(x1);
