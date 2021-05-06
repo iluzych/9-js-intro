97ca875e4259273aea4527105bf3e626727772d4
iluzych <ieva.daugvilaite@gmail.com>
2021-05-06 20:45:30 +0300

    Switch stoteles

--- js/10-switch.js
+++ js/10-switch.js
@@ -70,3 +70,75 @@ if (day >=0 && day < 6) {
 } else {
     console.log('Neatpazinta savaites diena');
 }
+
+console.log('----------------------');
+
+/*
+Arbatos paruosimo procesas:
+    0) paimti puodeli
+    1) isideti arbatos
+    2) isideti cukraus
+    3) uzpilti vandens
+    4) ismaisyti
+    5) gerti
+*/
+
+const processStep = 0;
+
+switch (processStep) {
+    case 0:
+        console.log('paimti puodeli');
+        
+    case 1:
+        console.log('isideti arbatos');
+
+    case 2:
+        console.log('isideti cukraus');
+
+    case 3:
+        console.log('uzpilti vandens');
+
+    case 4:
+        console.log('ismaisyti');
+
+    case 5:
+        console.log('gerti');
+        break;
+    
+    default:
+        console.log('Neatpazintas procesas');
+        break;
+
+}
+
+
+
+const stotele = 'Tremtiniu2';
+
+switch (stotele) {
+    case 'Geniu':
+        console.log('Geniu');
+    case 'Tremtiniu':
+        console.log('Tremtiniu');
+    case 'Rudens':
+        console.log('Rudens');
+    case 'K. Mindaugo':
+        console.log('K. Mindaugo');
+    case 'Z.Tiltas':
+        console.log('Z. Tiltas');
+        break;
+
+    case 'Lvovo':
+        console.log('Lvovo');
+    case 'Vasaros':
+        console.log('Vasaros');
+    case 'Rudens2':
+        console.log('Rudens2');
+    case 'Tremtiniu2':
+        console.log('Tremtiniu2');
+        break;
+        
+    default:
+        console.log('Neatpazinta stotele');
+        break;
+}
